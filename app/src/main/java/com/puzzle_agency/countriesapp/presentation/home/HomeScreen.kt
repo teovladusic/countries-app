5d32ab14ff3a2e7d7f16ea352f5416a6e939ce8e
Teo Vladušić <teo.vladusic@gmail.com>
2023-08-04 16:44:17 +0200

    setup navigation

---
+++ app/src/main/java/com/puzzle_agency/countriesapp/presentation/home/HomeScreen.kt
@@ -0,0 +1,14 @@
+package com.puzzle_agency.countriesapp.presentation.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+
+@RootNavGraph(start = true)
+@Destination
+@Composable
+fun HomeScreen() {
+
+    Text(text = "Home")
+}
